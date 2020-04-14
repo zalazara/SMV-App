86d372dbcb93ff0856b8ac5fff8949afdbd76bbe
Alejandro Zalazar <a307911>
2020-04-14 14:53:58 -0300

    Initial Commit

---
+++ SMV App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SMV App
+//
+//  Created by Zalazar, Alejandro Ramon on 14/04/2020.
+//  Copyright © 2020 Alejandro Zalazar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
